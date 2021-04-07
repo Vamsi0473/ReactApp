dba18804cf19b81fce976db7453da6f47c1f98b3
Vamsi <lakshmipuramvamsikrishna@gmail.com>
2021-04-07 00:56:49 -0400

    docker changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,3 @@ export class AppComponent implements OnInit {
 }
 
 
-/**  Copyright 2018 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
